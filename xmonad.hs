16b039d2dbffbf6b568ade56a6a0a69f556426d4
Thomas DuPlessis <thomasduplessis555@gmail.com>
2015-03-29 16:54:43 -0400

    up to date

--- xmonad.hs
+++ xmonad.hs
@@ -7,46 +7,63 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Layout.Spacing
 import XMonad.Config.Gnome
-import XMonad.Layout.Fullscreen
+import XMonad.Layout.Fullscreen  (fullscreenEventHook, fullscreenManageHook, fullscreenFull, fullscreenFloat)
+import XMonad.Hooks.ManageHelpers (doCenterFloat, (/=?), isInProperty, isFullscreen, (-?>), doFullFloat, composeOne)
 import XMonad.Layout.NoBorders
-   
-myLayout = avoidStruts  (tiled ||| Mirror tiled ||| Full) ||| noBorders (fullscreenFull Full)  
-     where  
-       -- default tiling algorithm partitions the screen into two panes  
-       tiled = smartSpacing 5 $ Tall nmaster delta ratio  
-   
-       -- The default number of windows in the master pane  
-       nmaster = 1  
-   
-       -- Default proportion of screen occupied by master pane  
-       ratio = 1/2  
-   
-       -- Percent of screen to increment by when resizing panes  
-       delta = 3/100  
-    
+
+myLayout = avoidStruts  (tiled ||| smallspacing ||| Mirror tiled ||| Full) ||| noBorders (fullscreenFull Full)
+     where
+       -- default tiling algorithm partitions the screen into two panes
+       tiled = smartSpacing 20 $ Tall nmaster delta ratio
+
+       smallspacing = smartSpacing 10 $ Tall nmaster delta ratio
+       -- The default number of windows in the master pane
+       nmaster = 1
+
+       -- Default proportion of screen occupied by master pane
+       ratio = 1/2
+
+       -- Percent of screen to increment by when resizing panes
+       delta = 3/100
+myManageHook = composeAll
+               [
+                className =? "Gimp" --> doFloat
+                , className =? "lighthouse" --> doFloat
+                , className =? "nemo" --> doFloat
+                , className =? "nautilus" --> doFloat
+                , className =? "Nautilus" --> doFloat
+                , className =? "nm-applet" --> doFloat
+               ]
+               <+>
+               composeOne [ isFullscreen -?> doFullFloat ]
+
+                          
+mybordercolor = "#606060"
+                
 main :: IO ()
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/thomasduplessis/.xmobarrc"
     --when load up set my background
     spawn "feh --bg-scale /home/thomasduplessis/Pictures/colorful-triangles-background.jpg &"
-    xmonad $ gnomeConfig
+    xmonad $ defaultConfig
         {
-          manageHook = manageDocks <+> manageHook defaultConfig,
+          manageHook =  myManageHook <+> manageDocks,
           layoutHook = myLayout,
               --smartSpacing 5 $ Tall 1 (3/100) (1/2),
           logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         },
-          
-          terminal = "gnome-terminal",
-          focusFollowsMouse  = False,
+
+          terminal = "urxvt",
+          focusFollowsMouse  = True,
+          focusedBorderColor = mybordercolor,
           keys =    keys defaultConfig,
           modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys` morekeys
 
 morekeys :: [((KeyMask, KeySym), X())]
-morekeys = [ 
+morekeys = [
        ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock"),
 
        -- print screens
@@ -55,15 +72,15 @@ morekeys = [
                  spawn "sleep 0.2; scrot window_%Y-%m-%d-%H-%M-%S.png -d 1-u"),
           -- print entire screen
        ((0, xK_Print), spawn "gnome-screenshot"),
-                                     
+
        -- This is a cool program that acts like Alfred for Mac
        ((mod4Mask, xK_x), spawn "lighthouse | sh"),
 
-       --toggle spaces in between windows 
+       --toggle spaces in between windows
        ((mod4Mask, xK_b), sendMessage ToggleStruts),
 
-       --my volume control 
-       ((0, 0x1008FF11), spawn "amixer set Master 2-"),       
+       --my volume control
+       ((0, 0x1008FF11), spawn "amixer set Master 2-"),
        ((0, 0x1008FF13), spawn "amixer set Master unmute" >>
                        spawn "amixer set Master 2+"),
        ((0, 0x1008FF12), spawn "amixer set Master toggle"),
