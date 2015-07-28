a07671c66cab3cff934392d1e8e5cb20759fe3f5
thomasDuPlessis <thomasduplessis>
2015-07-27 22:05:08 -0400

    updated bash scripts and new background

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
+#! /bin/bash
 # this file will create symlinks in the home dir
 
-files = .vimperatorrc .xmobarrc  .zshrc .gnus .Xdefaults .bashrc .gtkrc-2.0 .xsessionrc .themes/ .vimperator/ .oh-my-zsh/ .config/ .emacs.d/
+files=(.vimperatorrc .xmobarrc  .zshrc .gnus .Xdefaults .bashrc .gtkrc-2.0 .xsessionrc .themes/ .vimperator/ .oh-my-zsh/ .config/)
 
-for file in $files
+for file in ${files[*]}
 do
-    ln -s $file ~/
+    echo creating link to $file in $HOME
+    ln -sf $file ~/
 done
 
