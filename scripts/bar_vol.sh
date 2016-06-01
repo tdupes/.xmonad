67fa0d50d44106dc2338e0214e6c43d78e44552e
thomasDuPlessis <thomasduplessis555@gmail.com>
2016-05-31 21:32:51 -0400

    fixed volume (requires pulse), pacman, and volume, new screenshots

--- scripts/bar_vol.sh
+++ scripts/bar_vol.sh
@@ -2,20 +2,15 @@
 
 source $(dirname $0)/config.sh
 
-AMASTER=`amixer get Master | awk 'END{gsub(/\[|\]|%/,""); print $4}'`
+
+PERC=`pactl list sinks | grep Volume | head -1 |  awk -F"/" '{print $2}' | sed -e 's/^[ \t]*//'`
 ASTAT=`amixer get Master | awk 'END{gsub(/\[|\]|%/,""); print $6}'`
 ICON=""
 
 if [[ $ASTAT = "on" ]]; then
     ICON="audioHigh.xbm" #"spkr_01.xbm"
-    PERCBAR=`echo "$AMASTER"\
-        | gdbar -bg $bar_bg -fg $bar_fg -h 1 -w 50`
-		PERC=$AMASTER
 else
     ICON="audio-volume-muted.xbm"
-    PERCBAR=`echo 0 \
-        | gdbar -bg $bar_bg -fg $bar_fg -h 1 -w 50`
-		PERC=$AMASTER
 fi
 
 ICON='^i(/home/tom/.xmonad/dzen2/'"$ICON)"
