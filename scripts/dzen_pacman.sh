4a8957f077eb97145fc37a0a666e95bb7c5f8d5f
thomasDuPlessis <thomasduplessis555@gmail.com>
2016-06-05 15:29:51 -0400

    changed pacman button display

--- scripts/dzen_pacman.sh
+++ scripts/dzen_pacman.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source $(dirname $0)/config.sh
-XPOS=$((2935 + $XOFFSET))
-WIDTH="300"
-LINES=2
+XPOS=$((2570 + $XOFFSET))
+WIDTH="550"
+LINES=3
 
 updates=$(pacman -Qu | wc -l)
 packages=$(pacman -Qq  | wc -l)
 
-(echo "^fg($white)Updates"; echo "$updates"; echo " "; echo "^fg($white0) you have $packages packages";sleep 15) | dzen2 -fg $foreground -bg $background -fn $FONT -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button2=exec:urxvtc;button3=exit'
+(echo "^fg($white)Updates";  echo " "; echo "^fg($white0) You have $updates updates available out of  $packages packages";sleep 15) | dzen2 -fg $foreground -bg $background -fn $FONT -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button2=exec:urxvtc;button3=exit'
